e1ce6d5fa5c521e958f2a06d41a2f038bd176ff7
jax-parker <parkyjax711@gmail.com>
2023-06-28 19:44:32 +0000

    try again

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["djangoblog711.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["git.heroku.com/djangoblog711.git", "localhost"]
 
 
 # Application definition
